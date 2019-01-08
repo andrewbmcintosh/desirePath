21e01a592487f430233d9c8aec04e9a8ae43d3dd
andrewmcintosh <abmcintosh7@gmail.com>
2019-01-07 19:46:47 -0500

    changed syntax in views

--- controllers/pathController.js
+++ controllers/pathController.js
@@ -3,7 +3,7 @@ Path = require("../models/Path")
 const pathController = {
     index: (req, res) => {
         Path.find({}).then(paths => {
-            res.render('app/index', { links })
+            res.render('app/index', { paths })
         })
     },
     create: (req, res) => {
